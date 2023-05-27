cd0de13daf4890f6ec07f028e0410bbda0ed6115
mahendra <paudel.mahendra@gmail.com>
2023-05-27 22:59:45 +0545

    day3:bmi calculator

--- dart-basics/dart-project/bin/dart_project.dart
+++ dart-basics/dart-project/bin/dart_project.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:dart_project/dart_project.dart' as dart_project;
 import 'dart:io';
 
@@ -32,4 +34,27 @@ void main() {
   int intValue = int.parse(value.toString());
   int squareValue = intValue * intValue;
   print("Square of $intValue is equal to $squareValue");
+
+  /* calculate BMI */
+  double weight = 70;
+  double height = 1.6;
+  double bmi = weight / (height * height);
+  print("Your BMI is ${bmi.toStringAsFixed(2)}");
+  if (bmi < 16) {
+    print("Severe Thinness");
+  } else if (bmi < 17 && bmi > 16) {
+    print("Moderate thinness");
+  } else if (bmi > 17 && bmi < 18.5) {
+    print("Mild thiness");
+  } else if (bmi > 18.5 && bmi < 25) {
+    print("Normal");
+  } else if (bmi > 25 && bmi < 30) {
+    print("Overweight");
+  } else if (bmi > 30 && bmi < 35) {
+    print("Obese class I");
+  } else if (bmi > 35 && bmi < 40) {
+    print("Obese class II");
+  } else {
+    print("Obese class III");
+  }
 }
